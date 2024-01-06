c85667abe53e82c06c616d6c600b5c6c89bf06a6
Gabriel Reis <tharazo101@gmail.com>
2024-01-06 18:27:05 -0300

    Add findByEventId method to UserRepository and implement findUsersByEventId in NotificationService

--- src/main/java/com/gabrielreis/usernotification/repositories/UserRepository.java
+++ src/main/java/com/gabrielreis/usernotification/repositories/UserRepository.java
@@ -1,16 +1,24 @@
 package com.gabrielreis.usernotification.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gabrielreis.usernotification.entities.User;
 
 /**
  * This interface represents a repository for managing User entities.
- * It extends the JpaRepository interface, providing CRUD operations for User entities.
+ * It extends the JpaRepository interface, providing CRUD operations for User
+ * entities.
  * 
  * @see org.springframework.data.jpa.repository.JpaRepository
  * @author Gabriel Reis.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
 
-} 
+  @Query("SELECT u FROM User u JOIN u.event e WHERE e.id = :eventId")
+  public List<User> findByEventId(@Param("eventId") Long eventId);
+
+}
--- src/main/java/com/gabrielreis/usernotification/services/NotificationService.java
+++ src/main/java/com/gabrielreis/usernotification/services/NotificationService.java
@@ -1,17 +1,21 @@
 package com.gabrielreis.usernotification.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gabrielreis.usernotification.entities.User;
+import com.gabrielreis.usernotification.repositories.UserRepository;
 
 @Service
 public class NotificationService {
 
   @Autowired
-  private UserService userService;
+  private UserRepository userRepository;
 
-  NotificationService() {
-  
+  public List<User> findUsersByEventId (Long eventId) {
+    return userRepository.findByEventId(eventId);
   }
-
   
 }
