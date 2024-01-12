ac7021d8919d366f57ba9f3ed2a9d538ae83c038
Gabriel Reis <tharazo101@gmail.com>
2024-01-12 14:54:04 -0300

    Add Job interface and implement it in Notification class

--- src/main/java/com/gabrielreis/usernotification/notification/Notification.java
+++ src/main/java/com/gabrielreis/usernotification/notification/Notification.java
@@ -1,15 +1,34 @@
 package com.gabrielreis.usernotification.notification;
 
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.gabrielreis.usernotification.enums.Endpoints;
+import com.gabrielreis.usernotification.interfaces.Job;
 
 @Component
-public class Notification {
+public class Notification implements Job{
 
-  NotificationJob notificationJob = new NotificationJob();
+  Logger LOG = LoggerFactory.getLogger(Notification.class);
 
-  @Scheduled(fixedRate = 60000)
+  RestTemplate restTemplate = new RestTemplate();
+  final String URL = Endpoints.SEND_NOTIFICATION.url;
+
+  @Scheduled(fixedRate = 600000)
   public void sendNotification() {
-    notificationJob.execute();
+    execute();
+  }
+
+  @Override
+  public void execute() {
+    Objects.requireNonNull(restTemplate, "restTemplate is null");
+    Objects.requireNonNull(URL, "URL is null");
+    restTemplate.postForObject(URL, Object.class, Object.class);
+    LOG.info("Notification sent");
   }
 }
